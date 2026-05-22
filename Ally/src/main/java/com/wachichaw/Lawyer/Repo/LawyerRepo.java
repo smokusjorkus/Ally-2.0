ea14869ed63d95d2825d558d2bce0cf8435f6068
smokusjorkus <johannelumauag11@gmail.com>
2026-05-22 12:04:33 +0800

    initial commit

--- Ally/src/main/java/com/wachichaw/Lawyer/Repo/LawyerRepo.java
+++ Ally/src/main/java/com/wachichaw/Lawyer/Repo/LawyerRepo.java
@@ -1,6 +1,5 @@
 package com.wachichaw.Lawyer.Repo;
 
-
 import java.util.List;
 
 import org.springframework.data.jdbc.repository.query.Query;
@@ -11,14 +10,15 @@ import org.springframework.stereotype.Repository;
 import com.wachichaw.Lawyer.Entity.LawyerEntity;
 
 @Repository
-public interface LawyerRepo extends JpaRepository<LawyerEntity, Integer>{
+public interface LawyerRepo extends JpaRepository<LawyerEntity, Integer> {
     boolean existsByEmail(String email);
 
     List<LawyerEntity> findByCredentialsVerified(Boolean credentialsVerified);
 
     @Query("SELECT l FROM LawyerEntity l WHERE :specialization MEMBER OF l.specialization")
     List<LawyerEntity> findBySpecialization(@Param("specialization") String specialization);
-    
+
     long countByCredentialsVerifiedTrue();
+
     long countByCredentialsVerifiedFalse();
 }
