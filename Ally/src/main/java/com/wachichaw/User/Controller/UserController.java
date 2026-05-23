da4e2d8a2a13532b16766335236c0dddc0b6ed1a
smokusjorkus <johannelumauag11@gmail.com>
2026-05-23 15:47:52 +0800

    Fix missing ResponseStatusException import

--- Ally/src/main/java/com/wachichaw/User/Controller/UserController.java
+++ Ally/src/main/java/com/wachichaw/User/Controller/UserController.java
@@ -15,6 +15,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.google.cloud.storage.Bucket;
 import com.google.cloud.storage.Blob;
